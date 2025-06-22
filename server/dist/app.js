438cad22098983274c59413897813412ce14d4a7
Hasan Ali <hasanali5103@gmail.com>
2025-06-21 22:34:53 -0400

    more prod changes, likely more to come

--- server/dist/app.js
+++ server/dist/app.js
@@ -6,20 +6,34 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./auth.js";
 import spotifyRoutes from "./spotify.js";
 import dbRoutes from "./db.js";
+import { RedisStore } from "connect-redis";
+import { createClient } from "redis";
 dotenv.config();
+const client = createClient({
+    url: process.env.REDIS_URL,
+});
+client.on("error", function (err) {
+    throw err;
+});
+await client.connect();
+const redisStore = new RedisStore({
+    client: client,
+});
+client.ping().then(console.log).catch(console.error);
 const app = express();
-const PORT = 8080;
+const PORT = 10000;
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
 }));
 app.use(cookieParser());
 app.use(session({
+    store: redisStore,
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true,
     cookie: {
-        secure: false, // Set to true if using HTTPS
+        secure: process.env.NODE_ENV === "production", // Set to true if using HTTPS
         httpOnly: true,
         sameSite: "lax",
         maxAge: 24 * 60 * 60 * 1000, // 24 hours
@@ -30,5 +44,5 @@ app.use("/auth", authRoutes);
 app.use("/spotify", spotifyRoutes);
 app.use("/db", dbRoutes);
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running at ${PORT}`);
 });
--- server/src/app.ts
+++ server/src/app.ts
@@ -6,7 +6,28 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./auth.js";
 import spotifyRoutes from "./spotify.js";
 import dbRoutes from "./db.js";
+import { RedisStore } from "connect-redis";
+import { createClient } from "redis";
+
 dotenv.config();
+
+//is production
+const isProduction = process.env.NODE_ENV === "production";
+
+const client = createClient({
+  url: process.env.REDIS_URL,
+});
+
+client.on("error", function (err) {
+  throw err;
+});
+await client.connect();
+
+const redisStore = new RedisStore({
+  client: client,
+});
+
+client.ping().then(console.log).catch(console.error);
 
 const app = express();
 const PORT = 10000;
@@ -22,11 +43,12 @@ app.use(cookieParser());
 
 app.use(
   session({
+    store: isProduction ? redisStore : undefined,
     secret: process.env.SESSION_SECRET!,
     resave: true,
     saveUninitialized: true,
     cookie: {
-      secure: true, // Set to true if using HTTPS
+      secure: isProduction, // Set to true if using HTTPS
       httpOnly: true,
       sameSite: "lax",
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
